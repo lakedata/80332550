2a1651b45284e87e88b1dd6cfc8bf7f739d7f350
jung <wldud2550@naver.com>
2024-12-01 19:55:01 +0900

    ✨feat: H2 Database 연결 (#2)

---
+++ src/main/java/com/skmservice/domain/file/entity/File.java
@@ -0,0 +1,36 @@
+package com.skmservice.domain.file.entity;
+
+import com.skmservice.domain.post.entity.Post;
+import com.skmservice.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "file")
+public class File extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post; // Post 엔티티와의 다대일 관계
+
+    @Column(name = "file_name")
+    private String fileName;
+
+    @Column(name = "file_path")
+    private String filePath;
+
+    @Column(name = "file_type")
+    private String fileType;
+}
+//제목,작성자ID,조회수,첨부파일여부,
---
+++ src/main/java/com/skmservice/domain/member/entity/Member.java
@@ -0,0 +1,27 @@
+package com.skmservice.domain.member.entity;
+
+import com.skmservice.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "member")
+public class Member extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(name = "member_email", unique = true)
+    private String email;
+
+    @Column(name = "member_password")
+    private String password;
+}
